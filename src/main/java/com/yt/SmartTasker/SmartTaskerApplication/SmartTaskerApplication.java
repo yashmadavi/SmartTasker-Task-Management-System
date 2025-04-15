79a8ba152004f2f4d70cff64fcc581f3517aece0
Yash Madavi <147967454+yashmadavi@users.noreply.github.com>
2025-04-15 15:08:39 +0530

    Add files via upload

---
+++ src/main/java/com/yt/SmartTasker/SmartTaskerApplication/SmartTaskerApplication.java
@@ -0,0 +1,12 @@
+package com.yt.SmartTasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTaskerApplication.class, args);
+	}
+}
